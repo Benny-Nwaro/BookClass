29a04338ad3ffff2489718459840ce4ab7ca4294
Benny-Nwaro <baroh3767@aol.com>
2025-01-29 01:32:06 +0100

    update

--- src/app/bookclass/page.tsx
+++ src/app/bookclass/page.tsx
@@ -1,10 +1,13 @@
 // import BookingComponent from '@/components/BookingComponent'
 
+import Book from "../../components/Book"
 // import Home from '@/components/Home'
+Book
 export default function Page() {
     return (
         // <BookingComponent />
         // <Home/>
+        <Book/>
         
     )
 }
